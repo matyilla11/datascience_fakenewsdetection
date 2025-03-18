39254f3fc701ced55eecf11225912eb3a5c91d81
matyilla11 <matyi.attila11@gmail.com>
2025-03-18 15:05:20 +0100

    idontknowwhatimdoing

--- testgit.py
+++ testgit.py
@@ -1,12 +1,3 @@
 print("Hello word")
 print("Another helo")
 print("new blank file")
-
-<<<<<<< HEAD
-asdasdasdasdasdasasdasdasdasdasdasdasdasdas
-=======
-def print_something(a):
-    print(a)
-
-print_something("hello")
->>>>>>> bd2c16c23cbe8fdd19cf3e7785fd9a6669c59ce7
